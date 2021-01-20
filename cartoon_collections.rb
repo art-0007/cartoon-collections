4160cdf221b5a772b141ffbc44385de374b2322c
Artur <info@aigroupusa.com>
2021-01-20 18:33:06 -0500

    commit tree 69c345515ca83537b33e5ee187fffc827dd4ed27

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)# code an argument here
+  new_arr = []
+  array.each_with_index {|v, i| new_arr << "#{i+1}. #{v}"}
+  puts new_arr.join(" ")
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  array.collect {|x| x.capitalize + "!"}
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |x|  
+    if   cheese_types.include?(x)
+      return x
+    end
+    
+end
+return nil
 end
